e823194c071dfb803418353709fda3a47ca8cbe5
PavelErichev <dustfud@yandex.ru>
2022-10-25 23:04:43 +0300

    Init_3.1

---
+++ src/test/java/ru/erichev/spring/boot/web/CrudSpringBoot311ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.erichev.spring.boot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBoot311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
